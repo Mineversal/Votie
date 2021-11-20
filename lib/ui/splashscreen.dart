3fc73eec1a4fca8990294924e496f324fd5da416
Joe Hawk <azharrizkizulma@gmail.com>
2021-11-20 13:44:09 +0700

    Add Google Firebase Core & Splash Screen

---
+++ lib/ui/splashscreen.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  static const routeName = '/splashscreen';
+
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreen createState() => _SplashScreen();
+}
+
+class _SplashScreen extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    splashScreen();
+  }
+
+  splashScreen() {
+    var duration = const Duration(seconds: 5);
+    return Timer(duration, () {
+      //Navigator.pushReplacementNamed(context, Menu.routeName);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/logo.png", width: 200),
+            const SizedBox(height: 24.0),
+            Text(
+              "Votie",
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
