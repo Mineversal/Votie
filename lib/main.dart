896630fcdbef91f892be63c7904c8eccce2f46b9
Joe Hawk <azharrizkizulma@gmail.com>
2021-11-22 08:56:17 +0700

    Update yaml

--- lib/main.dart
+++ lib/main.dart
@@ -21,12 +21,12 @@ class MyApp extends StatelessWidget {
       routes: {
         SplashScreen.routeName: (context) => const SplashScreen(),
         /*
-          Menu.routeName: (context) => const Menu(),
-          Setting.routeName: (context) => const Setting(),
-          RestaurantDetailPage.routeName: (context) => RestaurantDetailPage(
-              restaurant:
-                  ModalRoute.of(context)?.settings.arguments as Restaurant),
-          */
+        Menu.routeName: (context) => const Menu(),
+        Setting.routeName: (context) => const Setting(),
+        RestaurantDetailPage.routeName: (context) => RestaurantDetailPage(
+            restaurant:
+                ModalRoute.of(context)?.settings.arguments as Restaurant),
+        */
       },
     );
   }
